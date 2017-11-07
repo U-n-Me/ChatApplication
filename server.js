56f275cb12a5b80f626e44b7baa4bddc70b02e46
U-n-Me <up11@iitbbs.ac.in>
2017-11-07 11:02:13 +0530

    Server Code Added

---
+++ server.js
@@ -0,0 +1,72 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var portNo = 8090;
+
+var server = http.createServer(function(req, res){
+  // We are not dealing with http now, so no need to
+  // implement anything here
+});
+
+server.listen(portNo, function(){
+  console.log("Server listening on port: "+ portNo);
+});
+
+// Let's start WebSocket Server
+wsServer = new WebSocketServer({
+  httpServer : server
+});
+
+function originIsAllowed(origin){
+  // logic
+  return true;
+}
+
+// whole server(app) data
+var clients = {};
+
+wsServer.on('request', function(request){
+  var origin = request.origin;
+  //console.log(request);
+  if(!originIsAllowed(origin)){
+    request.reject();
+    console.log(new Date() + " Rejected: Connection from " + origin + ".");
+    return;
+  }
+  var connection = request.accept(null, origin);
+  var owner = connection.socket._handle.owner._peername;
+  console.log(new Date() + " Accepted: Connection from " + owner + " .");
+  //var index = clients.push(connection) - 1;
+  var userName = null;
+
+  connection.on('message', function(message){
+    if(message.type === 'utf8'){
+      if(userName === null){
+        userName = message.utf8Data;
+        if(clients.hasOwnProperty(userName))
+          userName += '@'+Math.round(Math.random() * 0xFFFF);
+        connection.sendUTF(JSON.stringify({type: 'personal_log', time: new Date(), username: userName}));
+        clients[userName] = connection;
+        console.log(new Date + ": " + userName + " added.");
+        broadCast({type: 'broadcast_log', time: new Date(), message: userName + ' is active.'});
+      }
+      else{
+        var message = message.utf8Data;
+        console.log(new Date + ": " + userName + ": "+ message);
+        broadCast({type: 'message', sender: userName, time: new Date(), message: message});
+      }
+    }
+  });
+
+  connection.on('close', function(reasonCode, description) {
+    delete clients[userName];
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    broadCast({type: 'broadcast_log', time: new Date(), message: userName + ' is offline.'});
+    });
+
+    function broadCast(json){
+      for(var client in clients){
+        if(client !== userName)
+          clients[client].sendUTF(JSON.stringify(json));
+      }
+    }
+});
